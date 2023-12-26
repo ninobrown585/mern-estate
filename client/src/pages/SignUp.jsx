a4ec376d79db4a7bfe48a2153dc5622c973be0dc
Adrian Brown <ninobrown58518@gmail.com>
2023-12-25 23:30:50 -0600

    placeholder update

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,8 +6,8 @@ export default function SignOut() {
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form className="flex flex-col gap-4">
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='username' className='border p-3 rounded-lg' id='email' />
-        <input type="password" placeholder='username' className='border p-3 rounded-lg' id='password' />
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
       <div className='flex gap-2 mt-5'>
